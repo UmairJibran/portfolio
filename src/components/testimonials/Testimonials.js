1e5a4b3549adb875b3f2e3aee4dfa9bf7ef802e9
UmairJibran <umairjibran7@gmail.com>
2022-03-20 18:13:21 +0500

    limit body scope

--- src/components/testimonials/Testimonials.js
+++ src/components/testimonials/Testimonials.js
@@ -14,7 +14,7 @@ export default function Skills() {
     setActiveStep(step);
   };
   return (
-    <div className="py-12 bg-white">
+    <div className="py-12 bg-white testimonial-body">
       <div className="max-w-screen-lg mx-auto px-4 sm:px-6 lg:px-8 flex flex-col justify-between">
         <div className="text-center">
           <h3 className="text-3xl sm:text-5xl leading-normal font-extrabold tracking-tight text-gray-900">
