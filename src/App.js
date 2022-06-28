a73a339712c94f6ebdd16d57900b1b994b27f93c
Umair Jibran <me@umairjibran.com>
2022-06-29 00:41:53 +0500

    greet user

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {useEffect} from "react";
 import "./App.css";
 import Main from "./containers/Main";
 import { ThemeProvider } from "styled-components";
@@ -6,6 +7,16 @@ import { chosenTheme } from "./theme";
 import { GlobalStyles } from "./global";
 
 function App() {
+  useEffect(() => {
+    const apiUrl = "https://europe-west2-jibran-u-portfolio-analytics.cloudfunctions.net/greetings";
+
+    const greetUser = async () => {
+      await fetch(apiUrl)
+    }
+
+    greetUser();
+
+  } ,[])
   return (
     <ThemeProvider theme={chosenTheme}>
       <>
