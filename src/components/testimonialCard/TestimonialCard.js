8c6633590ba97e92cd42974a6f45aed16b77dc7c
Umair Jibran <me@umairjibran.com>
2022-07-22 03:01:02 +0500

    add social icons on testimonial

--- src/components/testimonialCard/TestimonialCard.js
+++ src/components/testimonialCard/TestimonialCard.js
@@ -5,6 +5,7 @@ import "pure-react-carousel/dist/react-carousel.es.css";
 import "./TestimonialCard.css";
 
 const TestimonialCard = ({ testimonial, index }) => {
+  const mailTo = `mailto:${testimonial.email}`;
   return (
     <Slide index={index} tabIndex="null">
       <div className="xl:w-1/2 lg:w-3/4 w-full mx-auto text-center testimonial-main">
@@ -47,6 +48,74 @@ const TestimonialCard = ({ testimonial, index }) => {
               )}
             </span>
           </p>
+          <span className="button-bar mx-auto text-center my-5 gap-10">
+            {testimonial.email ? (
+              <a href={mailTo} target="_blank" rel="noreferrer">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+                  />
+                </svg>
+              </a>
+            ) : (
+              <></>
+            )}
+            {testimonial.linkedin ? (
+              <a href={testimonial.linkedin} target="_blank" rel="noreferrer">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="icon icon-tabler icon-tabler-brand-linkedin"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  stroke-width="2"
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  {" "}
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />{" "}
+                  <rect x="4" y="4" width="16" height="16" rx="2" />{" "}
+                  <line x1="8" y1="11" x2="8" y2="16" />{" "}
+                  <line x1="8" y1="8" x2="8" y2="8.01" />{" "}
+                  <line x1="12" y1="16" x2="12" y2="11" />{" "}
+                  <path d="M16 16v-3a2 2 0 0 0 -4 0" />{" "}
+                </svg>
+              </a>
+            ) : (
+              <></>
+            )}
+            {testimonial.website ? (
+              <a href={testimonial.website} target="_blank" rel="noreferrer">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+                  />
+                </svg>
+              </a>
+            ) : (
+              <></>
+            )}
+          </span>
         </div>
       </div>
     </Slide>
--- src/components/testimonials/Testimonials.js
+++ src/components/testimonials/Testimonials.js
@@ -50,10 +50,18 @@ export default function Skills() {
                   role="button"
                   aria-label="previous slide"
                 >
-                  <img
-                    src="https://tuk-cdn.s3.amazonaws.com/can-uploader/testimonal-svg2.svg"
-                    alt="previous"
-                  />
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
                 </ButtonBack>
 
                 <ButtonNext
@@ -61,10 +69,20 @@ export default function Skills() {
                   aria-label="next slide"
                   className="cursor-pointer ml-2"
                 >
-                  <img
-                    src="https://tuk-cdn.s3.amazonaws.com/can-uploader/testimonial-svg3.svg"
-                    alt="next"
-                  />
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth={2}
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M9 5l7 7-7 7"
+                    />
+                  </svg>
                 </ButtonNext>
               </div>
             </CarouselProvider>
