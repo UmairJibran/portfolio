17f445741fbdd0480bdbac1b113edd7e8fe23a51
Umair Jibran <me@umairjibran.com>
2023-05-14 17:59:35 +0500

    prepend an icon before title

--- src/containers/roleTimeline/RoleTimeline.jsx
+++ src/containers/roleTimeline/RoleTimeline.jsx
@@ -10,10 +10,13 @@ const RoleTimeline = ({ theme, role, getExperienceString }) => {
   return (
     <div className="role-timeline-main flex relative pb-4">
       <div className="ml-9">
-        <h3 className="text-lg font-bold mb-1" style={{ color: theme.text }}>
-          {role.title}
+        <h3
+          className="text-base font-semibold mb-1"
+          style={{ color: theme.text }}
+        >
+          ⭐ {role.title}
         </h3>
-        <span className="text-zinc-100 text-base">
+        <span className="text-zinc-100 text-sm">
           {period} · {duration}
         </span>
         <br />
