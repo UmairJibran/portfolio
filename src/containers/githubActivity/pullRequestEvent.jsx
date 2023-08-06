222279ec2a8bc324b4f1a52a5625139acd95bf4d
UmairJibran <me@umairjibran.com>
2023-08-06 19:52:25 +0500

    Create pullRequestEvent.jsx

---
+++ src/containers/githubActivity/pullRequestEvent.jsx
@@ -0,0 +1,45 @@
+const PullRequestEventContainer = ({ event }) => {
+  return (
+    <div className="xl:w-1/3 md:w-1/2 p-4" bis_skin_checked="1">
+      <div className="border border-gray-200 p-6 rounded-lg" bis_skin_checked="1">
+        <div
+          className="w-10 h-10 inline-flex items-center justify-center rounded-full text-indigo-500 mb-4"
+          bis_skin_checked="1"
+        >
+          <svg
+            width="64px"
+            height="64px"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+            <g
+              id="SVGRepo_tracerCarrier"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            ></g>
+            <g id="SVGRepo_iconCarrier">
+              <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M7 8.83a3.001 3.001 0 1 0-2 0v6.34a3.001 3.001 0 1 0 2 0V8.83ZM6 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2Zm0 12a1 1 0 1 0 0 2 1 1 0 0 0 0-2ZM17 15.17a3.001 3.001 0 1 0 2 0V10.4A5.4 5.4 0 0 0 13.6 5h-.186l.293-.293a1 1 0 0 0-1.414-1.414l-2 2a1 1 0 0 0 0 1.414l2 2a1 1 0 1 0 1.414-1.414L13.414 7h.186a3.4 3.4 0 0 1 3.4 3.4v4.77ZM17 18a1 1 0 1 1 2 0 1 1 0 0 1-2 0Z"
+                fill="#6366f1"
+              ></path>
+            </g>
+          </svg>
+        </div>
+        <a href={"https://github.com/" + event.repo.name}>
+          <h2 className="text-lg text-gray-200 font-medium title-font mb-2">
+            {event.repo.name}
+          </h2>
+        </a>
+        <p className="leading-relaxed text-base">
+          {event.payload?.pull_request?.body || "Made it go buzz..."}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default PullRequestEventContainer;
