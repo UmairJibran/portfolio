7dd19cf00b827b2d1daee899cb3764310e0fc067
Umair Jibran <me@umairjibran.com>
2024-08-15 16:51:03 +0500

    wiped old portfolio, moved to NEXT

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export', // Outputs a Single-Page Application (SPA).
-    distDir: './build', // Changes the build output directory to `./dist/`.
-}
+const nextConfig = {};
 
-export default nextConfig
+export default nextConfig;
