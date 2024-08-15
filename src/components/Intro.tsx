e7be7696bb4d240db067b3e320f15ad8506d694a
Umair Jibran <me@umairjibran.com>
2024-08-15 19:23:51 +0500

    update intro text style

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -2,18 +2,24 @@
 
 import profile from '@/data/profile.json';
 import parse from 'html-react-parser';
+import { Sacramento } from 'next/font/google';
 
 import SocialStrip from '@/components/SocialStrip';
+
+const sacramento = Sacramento({
+  subsets: ['latin'],
+  weight: '400',
+});
 
 export default function Greetings() {
   return (
     <>
       <div id="intro">
-        <h1 className="greeting-text sacramento-regular">
+        <h1 className={`${sacramento.className} text-6xl my-4`}>
           hey there, {profile.name.toLowerCase()} here!
         </h1>
-        <p className="greeting-text-p subTitle">{profile.subTitle}</p>
-        <p className="">{parse(profile.extraInfo)}</p>
+        <p>{profile.subTitle}</p>
+        <p>{parse(profile.extraInfo)}</p>
         <SocialStrip />
       </div>
     </>
