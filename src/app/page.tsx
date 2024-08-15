ca82f9aed536fb4dfd891af578134ae3d49bad58
Umair Jibran <me@umairjibran.com>
2024-08-15 21:00:30 +0500

    made mobile friendly

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,10 @@ import Greetings from '@/components/Greetings';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col px-24 text-black">
+    <main className="flex min-h-screen flex-col px-10 md:px-24 text-black">
+      <span className="h-8" />
       <Greetings />
+      <span className="h-8" />
     </main>
   );
 }
--- src/components/Greetings.tsx
+++ src/components/Greetings.tsx
@@ -12,11 +12,11 @@ export default function Greetings() {
     <>
       <div
         id="greeting"
-        className="flex w-full h-full text-start items-center m-auto"
+        className="flex flex-col-reverse w-full h-full text-start items-center m-auto md:flex-row"
       >
         <Intro />
         <Image
-          className="hero-image w-full"
+          className="hero-image w-full ml-0 md:ml-10"
           alt="Cat Programmer"
           src={image}
           width={450}
