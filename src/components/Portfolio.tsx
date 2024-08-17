ae15d5f609fbfa41b02d6e6ddcd05167ae8f99eb
Umair Jibran <me@umairjibran.com>
2024-08-18 02:13:06 +0500

    add heading before projects

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -10,6 +10,9 @@ export default function Portfolio() {
         id="portfolio"
         className="w-full min-h-screen justify-center items-center my-4"
       >
+        <h1 className="text-3xl font-bold text-center my-10">
+          Things I&apos;ve Built
+        </h1>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
           {portfolio.map((item) => (
             <PortfolioDetail key={item.id} item={item} />
