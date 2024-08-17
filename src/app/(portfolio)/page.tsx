5e456f6944f58ec808c170cab644674d280fb86a
Umair Jibran <me@umairjibran.com>
2024-08-18 00:39:00 +0500

    create footer

--- src/app/(portfolio)/page.tsx
+++ src/app/(portfolio)/page.tsx
@@ -3,6 +3,7 @@
 import Greetings from "@/components/Greetings";
 import Portfolio from "@/components/Portfolio";
 import Experience from "@/components/Experience";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <Greetings />
       <Portfolio />
       <Experience />
+      <Footer />
       <span className="h-8" />
     </main>
   );
---
+++ src/components/Footer.tsx
@@ -0,0 +1,52 @@
+export default function Footer() {
+  return (
+    <>
+      <div className="container px-5 py-24 mx-auto flex sm:flex-nowrap flex-wrap">
+        <div className="lg:w-full md:w-1/2 bg-gray-100 rounded-lg sm:mr-10 p-10 flex flex-col items-center justify-center">
+          <h1 className="text-5xl">Got an idea in mind?</h1>
+          <h2 className="text-3xl">
+            Let&apos;s connect <span className="inline md:hidden">👇</span>
+            <span className="hidden md:inline">👉</span>
+          </h2>
+        </div>
+        <div className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+          <div className="relative mb-4">
+            <label for="name" className="leading-7 text-sm text-gray-600">
+              Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              className="w-full rounded border border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            />
+          </div>
+          <div className="relative mb-4">
+            <label for="email" className="leading-7 text-sm text-gray-600">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              className="w-full rounded border border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            />
+          </div>
+          <div className="relative mb-4">
+            <label for="message" className="leading-7 text-sm text-gray-600">
+              Message
+            </label>
+            <textarea
+              id="message"
+              name="message"
+              className="w-full rounded border border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+            ></textarea>
+          </div>
+          <button className="text-white bg-black py-2 px-6 border-2 focus:outline-none hover:bg-white hover:text-black border-white hover:border-black rounded text-lg">
+            Send 🚀
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
