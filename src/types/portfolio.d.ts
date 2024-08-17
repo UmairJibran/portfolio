5b502853df64efa84dea2340d26e0f5c3d77c2e9
Umair Jibran <me@umairjibran.com>
2024-08-18 01:04:57 +0500

    update portfolio interface

--- src/types/portfolio.d.ts
+++ src/types/portfolio.d.ts
@@ -4,19 +4,6 @@ export interface IPortfilioItem {
   url: string;
   description: string;
   logo: string;
-  category: string;
   source: string | null;
   caseStudy?: string;
-  technologies: Technology[];
-}
-
-export interface ITechnology {
-  skillName: string;
-  simpleIcon: string;
-  style: Style;
-}
-
-export interface IStyle {
-  color: string;
-  background: string;
 }
