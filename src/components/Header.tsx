60b343168e6be6cb9d5c4142211292b00f3ed2cc
Umair Jibran <me@umairjibran.com>
2024-08-22 19:25:53 +0500

    dynamise nav in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { usePathname } from "next/navigation";
+
 import { launchExternalUrl, swrFetcher } from "@/utils";
 import { Button, Popover } from "antd";
 import useSWR from "swr";
@@ -9,8 +11,11 @@ import "@/styles/header.scss";
 import profile from "@/data/profile.json";
 import { useState, useEffect } from "react";
 import { IGazaStats } from "@/types/gazaStats";
+import Link from "next/link";
 
 export default function Header() {
+  const path = usePathname().split("/")[1];
+  console.log(path);
   const [rotationDays, setRotationDays] = useState(0);
   const [rotationKilled, setRotationKilled] = useState(0);
   const [killedInGaza, setKilledInGaza] = useState(0);
@@ -44,16 +49,37 @@ export default function Header() {
     }
   }
 
+  const navComponents = [];
+
+  if (path !== "") {
+    navComponents.push(
+      <Link href="/">
+        <span className="mr-5 cursor-pointer">Home</span>
+      </Link>,
+    );
+  }
+
+  if (path !== "blogs") {
+    navComponents.push(
+      <Link href="/blogs">
+        <span className="mr-5 cursor-pointer">Blogs</span>
+      </Link>,
+    );
+  }
+
+  if (path !== "case-studies") {
+    navComponents.push(
+      <Popover content="Launching Soon">
+        <span className="mr-5 cursor-pointer">Case Studies</span>
+      </Popover>,
+    );
+  }
+
   return (
     <header className="text-gray-600 body-font sticky z-50 top-0 bg-white blurred-bg">
       <div className="container mx-auto flex flex-wrap p-1 lg:p-4 flex-col lg:flex-row items-center">
         <nav className="lg:w-1/5 flex-wrap items-center text-base lg:ml-auto hidden lg:flex">
-          <Popover content="Launching Soon">
-            <span className="mr-5 cursor-pointer">Blogs</span>
-          </Popover>
-          <Popover content="Launching Soon">
-            <span className="mr-5 cursor-pointer">Case Studies</span>
-          </Popover>
+          {navComponents}
         </nav>
         <div className="text-xs lg:text-md lg:w-3/5 bg-red-500 text-white p-2 text-center order-first lg:order-none">
           <span
