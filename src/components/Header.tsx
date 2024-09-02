b9602c0dc38591a50dc755ecb8ca0f7c25271815
Umair Jibran <me@umairjibran.com>
2024-09-02 10:46:53 +0500

    add event listener for resume button click

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -105,6 +105,7 @@ export default function Header() {
             onClick={() => launchExternalUrl(profile.resume)}
             type="text"
             className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-1"
+            data-umami-event="resume-button-clicked"
           >
             Resume
             <svg
